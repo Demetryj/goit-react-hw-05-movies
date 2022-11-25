e16e2ecda674b85297bce392acbca70c60bc56e0
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-11-26 00:16:42 +0300

    remove file

--- src/components/TrendingTodayMovies/MoviesItem.jsx
+++
@@ -1,9 +0,0 @@
-import { NavLink } from 'react-router-dom';
-
-export const MoviesList = ({ dataMovie }) => {
-  return (
-    <NavLink to="movies">
-      {dataMovie.title ? dataMovie.title : dataMovie.name}
-    </NavLink>
-  );
-};
