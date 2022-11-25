079e90d2827466b95e823a876709b5a497f00260
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-11-26 00:18:42 +0300

    Update TrendingTodayMovies.jsx
    додав спінер при запиті

--- src/components/TrendingTodayMovies/TrendingTodayMovies.jsx
+++ src/components/TrendingTodayMovies/TrendingTodayMovies.jsx
@@ -1,13 +1,17 @@
 import { useState, useEffect } from 'react';
+import { ThreeDots } from 'react-loader-spinner';
 import { fetchMovies } from '../../services/fetchMovies';
-import { MoviesList } from './MoviesItem';
+import { TrendingMoviesList } from './TrendingMoviesList';
 
 export const TrendingTodayMovies = () => {
   const [movies, setMovies] = useState([]);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     const fetch = async () => {
       try {
+        setLoaded(true);
+
         const movies = await fetchMovies(
           'https://api.themoviedb.org/3/trending/all/day?api_key=084c550b6f1767443109bcf4bcaee21b'
         );
@@ -15,20 +19,27 @@ export const TrendingTodayMovies = () => {
         setMovies([...movies]);
       } catch (error) {
         console.log(error.message);
+      } finally {
+        setLoaded(false);
       }
     };
     fetch();
   }, []);
 
   return (
-    <ul>
-      {movies.map(movie => {
-        return (
-          <li key={movie.id}>
-            <MoviesList dataMovie={movie} />
-          </li>
-        );
-      })}
-    </ul>
+    <main>
+      <TrendingMoviesList movies={movies} />
+      {loaded && (
+        <ThreeDots
+          height="80"
+          width="80"
+          radius="9"
+          color="blue"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{ justifyContent: 'center' }}
+          visible={true}
+        />
+      )}
+    </main>
   );
 };
