a325908e25462235058392e06180f0ac3c290301
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-11-26 00:19:17 +0300

    Update Movies.jsx
    додав спінер при запиті  та повідомлення при запиті, на який немає відповіді

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import { ThreeDots } from 'react-loader-spinner';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { fetchMovies } from 'services/fetchMovies';
 import { SearchBox } from 'components/SearchBox';
 import { SearchMoviesList } from 'components/SearchMovies';
@@ -7,6 +10,7 @@ import { SearchMoviesList } from 'components/SearchMovies';
 const Movies = () => {
   const [queryMovie, setQueryMovie] = useState('');
   const [movies, setMovies] = useState([]);
+  const [loaded, setLoaded] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
   const query = searchParams.get('query') ?? '';
 
@@ -17,11 +21,25 @@ const Movies = () => {
           return;
         }
 
+        setLoaded(true);
+
         const moviesList = await fetchMovies(
           `https://api.themoviedb.org/3/search/movie?api_key=084c550b6f1767443109bcf4bcaee21b&query=${query}&language=en-US&page=1&include_adult=false`
         );
+
+        if (moviesList.length < 1) {
+          toast.info(
+            `Sorry, nothing found for your search "${query}" ! Try another query.`
+          );
+          return;
+        }
+
         setMovies([...moviesList]);
-      } catch (error) {}
+      } catch (error) {
+        console.log(error.mesage);
+      } finally {
+        setLoaded(false);
+      }
     };
 
     searchQuery();
@@ -29,8 +47,10 @@ const Movies = () => {
 
   const handleSubmit = event => {
     event.preventDefault();
+    const normalizedValue = event.target.elements.query.value.toLowerCase();
+
     const nextSearchParams =
-      queryMovie !== '' ? { query: event.target.elements.query.value } : {};
+      queryMovie !== '' ? { query: normalizedValue } : {};
     setSearchParams(nextSearchParams);
   };
 
@@ -42,6 +62,18 @@ const Movies = () => {
         setQueryMovie={setQueryMovie}
       />
       {query && <SearchMoviesList movies={movies} />}
+      {loaded && (
+        <ThreeDots
+          height="80"
+          width="80"
+          radius="9"
+          color="blue"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{ justifyContent: 'center' }}
+          visible={true}
+        />
+      )}
+      <ToastContainer />
     </main>
   );
 };
