9b0212b0a36df9078fee2dd87bddf39069e2acbb
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-11-27 00:23:51 +0300

    Update fetchMovies.js

--- src/services/fetchMovies.js
+++ src/services/fetchMovies.js
@@ -3,7 +3,5 @@ import axios from 'axios';
 export const fetchMovies = async url => {
   const response = await axios.get(url);
 
-  const { results } = response.data;
-
-  return results;
+  return response.data;
 };
