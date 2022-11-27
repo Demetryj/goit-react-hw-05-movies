2f49dcfe6d8aef8d2b0f7929d6c02900b4fca2c8
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-11-28 00:35:18 +0300

    Update MoviesList.jsx
    add location

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,17 +1,20 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 export const MoviesList = ({ movies, linkTo = false }) => {
+  const location = useLocation();
+  // console.log(location);
+
   return (
     <ul>
       {movies.map(movie => {
         return (
           <li key={movie.id}>
             {linkTo ? (
-              <NavLink to={`${linkTo}/${movie.id}`}>
+              <NavLink to={`${linkTo}/${movie.id}`} state={{ from: location }}>
                 {movie.title ? movie.title : movie.name}
               </NavLink>
             ) : (
-              <NavLink to={`${movie.id}`}>
+              <NavLink to={`${movie.id}`} state={{ from: location }}>
                 {movie.title ? movie.title : movie.name}
               </NavLink>
             )}
