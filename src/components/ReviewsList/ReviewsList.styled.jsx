838138247bcb33f86b8d8e55460972122fc9ce7d
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-11-29 23:56:03 +0300

    Create ReviewsList.styled.jsx

---
+++ src/components/ReviewsList/ReviewsList.styled.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Item = styled.li`
+  margin-bottom: ${props => props.theme.space[5]}px;
+
+  list-style: none;
+`;
+
+export const Author = styled.p`
+  font-family: ${props => props.theme.fonts.heading};
+  font-weight: ${props => props.theme.fontWeights.heading};
+  font-size: ${props => props.theme.fontSizes.m};
+  margin-bottom: ${props => props.theme.space[3]}px;
+`;
+
+export const Content = styled.p`
+  font-family: ${props => props.theme.fonts.body};
+  font-size: ${props => props.theme.fontSizes.s};
+  color: ${props => props.theme.colors.primaryText};
+
+  text-indent: ${props => props.theme.space[5]}px;
+  text-align: justify;
+`;
