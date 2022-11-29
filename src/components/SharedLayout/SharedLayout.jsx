277d4e2c13020ee03f97772ad51d9b0ffaf95a62
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-11-29 15:47:26 +0300

    Update SharedLayout.jsx
    add styles

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,19 +1,32 @@
 import { Suspense } from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import Box from 'components/Box';
+import { Navigation, NavLinkItem } from 'components/SharedLayout';
 
 const SharedLayout = () => {
   return (
-    <div>
+    <>
       <header>
-        <nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="movies">Movies</NavLink>
-        </nav>
+        <Box
+          maxWidth={1200}
+          px={[4]}
+          py={[3]}
+          boxShadow="third"
+          my={[0]}
+          mx="auto"
+          bg="backgroundHeader"
+          as="div"
+        >
+          <Navigation>
+            <NavLinkItem to="/">Home</NavLinkItem>
+            <NavLinkItem to="movies">Movies</NavLinkItem>
+          </Navigation>
+        </Box>
       </header>
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
-    </div>
+    </>
   );
 };
 
