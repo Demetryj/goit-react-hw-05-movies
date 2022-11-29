46e949c95f7a60a30aa8a24e037c47e280404b66
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-11-29 23:55:44 +0300

    Create Container.jsx

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+
+  padding-top: ${props => props.theme.space[4]}px;
+  padding-bottom: ${props => props.theme.space[5]}px;
+  padding-left: ${props => props.theme.space[4]}px;
+  padding-right: ${props => props.theme.space[4]}px;
+`;
