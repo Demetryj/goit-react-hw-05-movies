2180e93a81e946c31fe083dc7499911bd258b217
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-12-01 00:21:01 +0300

    Create SearchBox.styled.jsx

---
+++ src/components/SearchBox/SearchBox.styled.jsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  text-align: center;
+  margin-bottom: ${props => props.theme.space[5]}px;
+`;
+
+export const Input = styled.input`
+  margin-right: ${props => props.theme.space[3]}px;
+
+  width: 300px;
+  height: 30px;
+
+  border-radius: ${props => props.theme.radii.normal};
+
+  &:not(:placeholder-shown) {
+    border-color: ${props => props.theme.colors.accent};
+  }
+`;
+
+export const BtnSearch = styled.button`
+  padding-left: ${props => props.theme.space[3]}px;
+  padding-right: ${props => props.theme.space[3]}px;
+  height: 30px;
+  color: ${props => props.theme.colors.primaryText};
+  background-color: ${props => props.theme.colors.backgroundBtn};
+
+  border: ${props => props.theme.radii.none};
+  border-radius: ${props => props.theme.radii.normal};
+  cursor: pointer;
+
+  transition: color 250ms, background-color 250ms;
+
+  :hover,
+  :focus {
+    color: ${props => props.theme.colors.white};
+    background-color: ${props => props.theme.colors.backgroundAccentBtn};
+  }
+`;
