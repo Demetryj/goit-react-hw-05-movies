bf60303bcdf014d340b0496349d248f77f7073e2
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-12-02 21:30:15 +0300

    Update useMovieDetails.js

--- src/hooks/useMovieDetails.js
+++ src/hooks/useMovieDetails.js
@@ -3,14 +3,21 @@ import { useParams } from 'react-router-dom';
 import { fetchMovies } from 'services/fetchMovies';
 
 export const useMovieDetails = () => {
-  const [movie, setMovie] = useState([]);
+  const [movie, setMovie] = useState(null);
   const [loaded, setLoaded] = useState(false);
   const [error, setError] = useState(null);
   const { movieId } = useParams();
 
+  // const isFirstRender = useRef(true);
+
   useEffect(() => {
     const fetch = async () => {
       try {
+        // if (isFirstRender.current) {
+        //   isFirstRender.current = false;
+        //   return;
+        // }
+
         setLoaded(true);
 
         const dataMovie = await fetchMovies(
